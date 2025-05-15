45fe68e26e1af42f7c89b964ae26c3c07e28457c
Renaud Genevois <renaud.genevois@insee.fr>
2025-05-15 15:41:28 +0200

    init

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import react from "@vitejs/plugin-react";
+import dts from "vite-plugin-dts";
+
+export default defineConfig({
+  plugins: [react(), dts({ include: ["lib"] })],
+  build: {
+    copyPublicDir: false,
+    lib: {
+      entry: resolve(__dirname, "lib/main.ts"),
+      name: "lunatic",
+      fileName: "lunatic",
+
+      formats: ["es"],
+    },
+    rollupOptions: {
+      external: ["react", "react/jsx-runtime"],
+    },
+  },
+});
