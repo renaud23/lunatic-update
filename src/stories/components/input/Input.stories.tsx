3c2d95c8cf69a6e63a01c9ba08cddffad2ed19e8
Renaud Genevois <renaud.genevois@insee.fr>
2025-05-22 15:15:53 +0200

    external data for story Input

--- src/stories/components/input/Input.stories.tsx
+++ src/stories/components/input/Input.stories.tsx
@@ -1,12 +1,13 @@
 import { LunaticInput as Input } from '@lib/components/input/lunatic-input'
 import type { LunaticSource } from '@lib/use-lunatic/type-source'
 import { OrchestratorForStories } from '@src/utils/OrchestratorForStories'
-import { getVariableValue } from '@src/utils/tools'
 import type { Meta, StoryObj } from '@storybook/react'
 
+import data from './data.json'
 import source from './source.json'
 
 const meta: Meta<typeof Input> = {
+  title: 'Components/Input',
   component: OrchestratorForStories,
 }
 
@@ -17,10 +18,6 @@ type Story = StoryObj<typeof OrchestratorForStories>
 export const Primary: Story = {
   args: {
     source: source as unknown as LunaticSource,
-    data: {
-      COLLECTED: {
-        NOM: getVariableValue('Renaud'),
-      },
-    },
+    data,
   },
 }
